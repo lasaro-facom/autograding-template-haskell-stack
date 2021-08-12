16095cbc568c5a1aebe185b638dcaee3d616fd4e
lasaro <lasaro@users.noreply.github.com>
2021-08-12 10:34:44 -0300

    semana 4

--- src/Exercise.hs
+++ src/Exercise.hs
@@ -1,151 +1,55 @@
 module Exercise where
 
--- Defina os tipos das seguintes funções de acordo com as descrições.
-
 
 {-
-- entrada: três números inteiros de precisão finita (Int)
-- saída: Int
+Assim como Bool pode assumir os valores True e False, o tipo Naipe pode assumir um dos valores Copas, Espada, Ouro e Paus, tal que
+Copas < Espada < Ouro < Paus
+
+>>>Copas > Espada
+False
+
+>>> Espada > Ouro
+False
+
+>>> Paus > Copas
+True
+
+>>>maiorDeTres Copas Espada Paus
+Paus
+
+>>>maiorDeTres Copas Copas Copas
+Copas
+
 -}
-somaTresNumeros :: Num a => a -> a -> a -> a
-somaTresNumeros n1 n2 n3 = n1 + n2 + n3
+data Naipe = Copas | Espada | Ouro | Paus deriving (Ord,Eq,Show)
 
-{-
-Esta função retorna a área do quadrado de lado l
-
-areaQuadrado
-- Entrada: l, Float
-- Saída: l**2, Float
--}
-areaQuadrado :: Floating a => a -> a
-areaQuadrado l = l**2
-
-{-
-Esta função retorna a área do retângulo de lado l1 e l2
-
-areaRetangulo
-- Entrada: l1 e l2, Floats
-- Saída: l1 * l2, Float
--}
-areaRetangulo :: Num a => a -> a -> a
-areaRetangulo l1 l2 = l1 * l2
-
-
--- Defina as seguintes funções usando if then else
-{-
-Esta função retorna o maior de tres números
-
-maiorDeTres
-- Entrada: a b c, Integer
-- Saída: o maior dentre a b c, Integer
--}
-maiorDeTres :: Ord a => a -> a -> a -> a
-maiorDeTres a b c
-  | a >= b && a >= c = a
-  | b >= c = b
-  | otherwise = c
-
-
--- Defina as seguintes funções, incluindo a definição de tipos.
-
-{-
-Esta função calcula quem ganha no jogo pedra/tesoura/papel
-
-pedraTesouraPapel
-- Entrada: m1 m2 - "pedra" "tesoura" "papel"
-- Saída: O valor ganhador, ou "empate" no caso empate
-
->>>pedraTesouraPapel "pedra" "papel"
-"papel"
-
->>>pedraTesouraPapel "pedra" "tesoura"
-"pedra"
-
->>>pedraTesouraPapel "papel" "tesoura"
-"tesoura"
--}
-pedraTesouraPapel :: String -> String -> String
-pedraTesouraPapel m1 m2
-  | m1 == "pedra" && m2 == "papel" = "papel"
-  | m1 == "pedra" && m2 == "tesoura" = "pedra"
-  | m1 == "papel" && m2 == "pedra" = "papel"
-  | m1 == "papel" && m2 == "tesoura" = "tesoura"
-  | m1 == "tesoura" && m2 == "papel" = "tesoura"
-  | m1 == "tesoura" && m2 == "pedra" = "pedra"
-  | otherwise = "empate"
-
-
-{-
-Esta função calcula a conjunção do dois parâmetros (e lógico).
--}
-eLógico :: Bool -> Bool -> Bool
-eLógico p1 p2 = p1 && p2
-
-{-
-Esta função calcula a disjunção do dois parâmetros (ou lógico).
--}
-ouLógico :: Bool -> Bool -> Bool
-ouLógico p1 p2 = p1 || p2
-
--- Defina as seguintes funções usando ++ !! take e reverse.
-
-{-
-Defina função que retorne substring de t elementos começando na posição i
-subStringDeAte "entrada1" 2 2 retorna "tr"
-
-Entrada:
-    - s: string
-    - i: inteiro
-    - t: inteiro
--}
-subStringDeAte :: String -> Int -> Int -> String
-subStringDeAte s i t = reverse (take maxT (reverse (take (i + maxT) s)))
-    where maxT = min t (length s - i)
-
-{-
-Defina função que retorne substring com os últimos u elementos
-
-Entrada:
-    - s: string
-    - u: inteiro
--}
-últimosU :: String -> Int -> String
-últimosU s u = reverse (take u (reverse s))
-
-{-
-Defina função que receba duas strings e retorne a resultado da concatenação das substrings de t elementos começando na posição i
-
-Entrada
-    - s1: string
-    - s2: string
-    - i: inteiro
-    - u: inteiro
--}
-
-subStringDeAteAppend :: String -> String -> Int -> Int -> String
-subStringDeAteAppend s1 s2 i u = subStringDeAte s1 i u ++ subStringDeAte s2 i u
-
-
-
--- Nas próximas funções, trabalharemos com definições de tipo e tuplas.
 
 {-
 Seja o tipo de dados Carta tupla em que 
  - o primeiro elemento é o valor da carta (1,2,3,4,5,6,7,8,9,10,11,12,13) 
- - o segundo é o naipe ("ouro", "copas", "espada", "paus").
+ - o segundo é um Naipe
 -}
-type Carta = (Int, String)
+type Carta = (Int, Naipe)
+
+
+-- Defina as seguintes funções usando apenas casamento de padrões do lado esquerdo das equações.
 
 {-
-Uma função que receba uma carta retorne seu naipe.
+Uma função que receba uma carta retorne seu Naipe.
 
 Entrada:
     - c1: carta
 
-Resultado: naipe da carta
+Resultado: 
+    - naipe da carta
+
+Exemplos:
+>>>naipe (1,Ouro)
+Ouro
 -}
-naipe :: Carta -> String
+naipe :: Carta -> Naipe
 naipe (_,n) = n
+
 
 {-
 Uma função que receba uma carta retorne seu valor.
@@ -153,128 +57,142 @@ Uma função que receba uma carta retorne seu valor.
 Entrada:
     - c1: carta
 
-Resultado: valor da carta
+Resultado: 
+    - valor da carta
+
+Exemplos:
+>>>valor (1,Ouro)
+1
 -}
 valor :: Carta -> Int
 valor (v,_) = v
 
 
 {-
-Uma função que receba duas cartas e diga se a primeira é menor que a segunda.
-Uma carta c1 é menor que uma carta c2 se valor c1 < valor2 OU se valor c1 == valor c2 e naipe c1 < c2.
-"copas" < "espada" < "ouro" < "paus"
+Uma função que receba duas cartas retorne se seus naipes são iguais.
 
 Entrada:
-    - c1, c2: Carta
-Resultado: True ou False
+    - c1: carta
+    - c2: carta
+
+Resultado: 
+    - naipe de c1 igual a naipe de c2?
+
+Exemplos:
+>>>naipeIgual (1,Ouro) (2,Ouro)
+True
+>>>naipeIgual (1,Ouro) (2,Paus)
+False
 -}
 
-cartaÉMenor :: Carta -> Carta -> Bool
-cartaÉMenor c1 c2 = c1 < c2
+naipeIgual :: Carta -> Carta -> Bool
+naipeIgual (_,c1n) (_,c2n) = c1n == c2n
 
 {-
-Uma função que receba duas cartas e diga se a primeira é igual à segunda.
+Uma função que receba uma carta e retorne seu valor por extenso.
 
 Entrada:
-    - c1, c2: Carta
-Resultado: True ou False
+    - c1: carta
+
+Resultado: 
+    - valor de c1 por extenso
+
+Exemplos:
+>>>valorPorExtenso (1,Ouro)
+"Um"
+>>>valorPorExtenso (2,Paus)
+"Dois"
+>>>valorPorExtenso (12,Ouro)
+"Dama"
 -}
 
-cartaÉIgual :: Carta -> Carta -> Bool
-cartaÉIgual c1 c2 = c1 == c2
+
+valorPorExtenso :: Carta -> String
+valorPorExtenso (1,_) = "Um"
+valorPorExtenso (2,_) = "Dois"
+valorPorExtenso (3,_) = "Três"
+valorPorExtenso (4,_) = "Quatro"
+valorPorExtenso (5,_) = "Cinco"
+valorPorExtenso (6,_) = "Seis"
+valorPorExtenso (7,_) = "Sete"
+valorPorExtenso (8,_) = "Oito"
+valorPorExtenso (9,_) = "Nove"
+valorPorExtenso (10,_) = "Dez"
+valorPorExtenso (11,_) = "Valete"
+valorPorExtenso (12,_) = "Dama"
+valorPorExtenso (13,_) = "Rei"
 
 {-
-Uma função que receba duas cartas e diga se a primeira é maior que a segunda.
+Uma função que receba uma carta e retorne seu naipe por extenso.
 
 Entrada:
-    - c1, c2: Carta
-Resultado: True ou False
--}
-cartaÉMaior :: Carta -> Carta -> Bool
-cartaÉMaior c1 c2 = c1 > c2
+    - c1: carta
 
+Resultado: 
+    - naipe de c1 por extenso
+
+Exemplos:
+>>>naipePorExtenso (1,Ouro)
+"Ouro"
+>>>naipePorExtenso (2,Paus)
+"Paus"
+>>>naipePorExtenso (12,Ouro)
+"Ouro"
+-}
+naipePorExtenso :: Carta -> String
+naipePorExtenso (_,n) = show n
 
 {-
-Uma função que receba três cartas c1 c2 c3 e diga se formam um jogo.
-- seja m1 a maior dentre as cartas c1 c2 c3
-- seja m2 a de valor mediano dentre as cartas c1 c2 c3
-- seja m3 a menor dentre as cartas c1 c2 c3
-m1 m2 e m3 formam um jogo se e somente si
-    - OU naipe m1 == naipe m2 == naipe m3 E valor m1 == valor m2 + 1 == valor m3 + 2
-    - OU naipe m1 =/= naipe m2 =/= naipe m3 =/= m1 E valor m1 == valor m2 == valor m3
-
+Uma função que receba três cartas e retorne um booleano dizendo se formam uma sequencia, isto é, se estão
+aparecem dentro da seguinte sequência: Espada Copas Ouro Paus Espada Copas
 
 Entrada:
-    - c1, c2, c3: Carta
-Resultado: True ou False
+    - Carta
+    - Carta
+    - Carta
+
+Resultado: 
+    - Estão em sequência?
+
+Exemplos:
+>>>sequênciaDeNaipes (1,Paus) (2,Ouro) (7,Copas)
+False
+
+>>>sequênciaDeNaipes (1,Paus) (7,Copas) (2,Ouro) 
+False
+
+>>>sequênciaDeNaipes (1,Paus) (2,Espada) (7,Copas)
+True
+
+>>>sequênciaDeNaipes (1,Espada) (2,Copas) (7,Ouro)
+True
 -}
-éJogo :: Carta -> Carta -> Carta -> Bool
-éJogo c1 c2 c3
-    |naipe c1 == naipe c2 && naipe c2 == naipe c3 && valor (maiorDeTresC c1 c2 c3) == valor (medianaDeTres c1 c2 c3) + 1 && valor (medianaDeTres c1 c2 c3) == valor (menorDeTres c1 c2 c3) + 1 = True
-    |naipe c1 /= naipe c2 && naipe c2 /= naipe c3 && naipe c3 /= naipe c2 && valor c1 == valor c2 && valor c2 == valor c3 = True
-    |otherwise = False
-
-
-maiorDeTresC :: Carta -> Carta -> Carta -> Carta
-maiorDeTresC c1 c2 c3
-    | c1 >= c2 && c1 >= c3 = c1
-    | c2 >= c3 = c2
-    | otherwise = c3
-
-menorDeTres :: Carta -> Carta -> Carta -> Carta
-menorDeTres c1 c2 c3
-    | c1 <= c2 && c1 <= c3 = c1
-    | c2 <= c3 = c2
-    | otherwise = c3
-
-cartaPraNumero :: Carta -> Int
-cartaPraNumero (v, n)
-    | n == "copas" = (v-1) * 4 + 1 
-    | n == "espada" = (v-1) * 4 + 2
-    | n == "ouro" = (v-1) * 4 + 3
-    | otherwise = (v-1) * 4 + 4
-
-numeroPraCarta :: Int -> Carta
-numeroPraCarta i
-    | (i-1) `rem` 4 == 0 = ((i-1) `div` 4 +1,"copas")
-    | (i-1) `rem` 4 == 1 = ((i-1) `div` 4 +1,"espada")
-    | (i-1) `rem` 4 == 2 = ((i-1) `div` 4 +1,"ouro")
-    | otherwise  = ((i-1) `div` 4 +1,"paus")
-
-
-medianaDeTres :: Carta -> Carta -> Carta -> Carta
-medianaDeTres c1 c2 c3 = numeroPraCarta (cartaPraNumero c1 + cartaPraNumero c2 + cartaPraNumero c3 - cartaPraNumero aMenor - cartaPraNumero aMaior)
-    where aMenor = menorDeTres c1 c2 c3
-          aMaior = maiorDeTres c1 c2 c3
+sequênciaDeNaipes :: Carta -> Carta -> Carta -> Bool
+sequênciaDeNaipes (_,Espada) (_,Copas) (_,Ouro) = True
+sequênciaDeNaipes (_,Copas) (_,Ouro) (_,Paus) = True
+sequênciaDeNaipes (_,Ouro) (_,Paus) (_,Espada) = True
+sequênciaDeNaipes (_,Paus) (_,Espada) (_,Copas)= True
+sequênciaDeNaipes (_,_) (_,_) (_,_) = False 
 
 {-
-Defina uma função que receba duas tuplas de 3 cartas, onde a primeira carta é maior ou igual à segunda, que é maior ou igual à terceira, 
-e compare as tuplas para dizer qual é maior.
-Uma tupla t1 é menor que uma tupla t2 se e somente se
-- a primeira carta de t1 é menor que a primeira de t2 OU
-- as primeiras cartas são iguais mas a segunda carta de t1 é menor que a segunda de t2
-- as primeiras e segundas cartas são iguais mas a terceira carta de t1 é menor que a terceira de t2
--}
-maiorMão :: (Carta, Carta, Carta) -> (Carta, Carta, Carta) -> (Carta, Carta, Carta)
-maiorMão t1 t2
-    |t1 >= t2 = t1
-    |otherwise = t2
+Uma função que recebe uma data na forma de três inteiros e retorna a da por extenso.
 
+Entrada:
+    - Dia
+    - Mês
+    - Ano
 
+Resultado
+    - Data por extenso
+    - Quando o dia for 1, usar Primeiro como extenso.
 
+Exemplos:
+>>>dataPorExtenso 1 1 2001
+"Primeiro de Janeiro de 2001"
 
-{-
-Defina uma função que receba duas tuplas de 3 cartas, onde a primeira carta é maior ou igual à segunda, que é maior ou igual à terceira, 
-e compare as tuplas para dizer qual ganha, ou se houve empate.
-Uma tupla t1 ganha de uma tupla t2 se 
-- t1 é um jogo mas t2 não é um jogo
-- t1 e t2 são um jogo mas t1 é maior que t2.
-- No caso de empate, retorne a tupla vazia ((0,""),(0,""),(0,""))
+>>>dataPorExtenso 10 3 2010
+"Dez de Março de 2010"
 -}
 
-mãoGanhadora :: (Carta, Carta, Carta) -> (Carta, Carta, Carta) -> (Carta, Carta, Carta)
-mãoGanhadora (ce1, ce2, ce3) (cd1, cd2, cd3)
-    |éJogo ce1 ce2 ce3 && not (éJogo cd1 cd2 cd3) = (ce1, ce2, ce3)
-    |not (éJogo ce1 ce2 ce3) && éJogo cd1 cd2 cd3 = (cd1, cd2, cd3)
-    |(ce1, ce2, ce3) == (cd1, cd2, cd3) = ((0,""),(0,""),(0,""))
-    |otherwise = maiorMão (ce1, ce2, ce3) (cd1, cd2, cd3)
+dataPorExtenso :: Int -> Int -> Int -> String
+dataPorExtenso dia mes ano = error "Implementar"
--- test/Tests.hs
+++ test/Tests.hs
@@ -8,100 +8,40 @@ main = hspecWith defaultConfig specs
 
 specs :: Spec
 specs = do
-          describe "Teste de funções simples" $ do
-            it "Soma tres números Int " $
-              somaTresNumeros (1::Int) (2::Int) (3::Int) `shouldBe` (6::Int)
-            it "Calcula a area do quadrado " $
-              areaQuadrado (2.0::Float) `shouldBe` (4::Float)
-            it "Calcula a area do retangulo " $
-              areaRetangulo (2::Float) (3::Float) `shouldBe` (6::Float)
-            it "Retorna o maior de 3 números" $
-              maiorDeTres (1::Integer) (2::Integer) (3::Integer) `shouldBe` (3::Integer)
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapel  ("pedra"::String) ("tesoura"::String) `shouldBe` ("pedra"::String)
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapel  ("papel"::String) ("tesoura"::String) `shouldBe` ("tesoura"::String)
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapel  ("papel"::String) ("papel"::String) `shouldBe` ("empate"::String)
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapel  ("papel"::String) ("pedra"::String) `shouldBe` ("papel"::String)
-            it "E logico" $
-              eLógico  True True `shouldBe` True
-            it "E logico" $
-              eLógico  True False `shouldBe` False
-            it "OU logico" $
-              ouLógico  True False `shouldBe` True
-            it "OU logico" $
-              ouLógico  False False `shouldBe` False
-            it "subStringDeAte" $
-              subStringDeAte ("entrada1"::String) (2::Int) (2::Int) `shouldBe` ("tr"::String)
-            it "subStringDeAte" $
-              subStringDeAte ("entrada1") (2::Int) (20::Int) `shouldBe` "trada1"
-            it "últimosU" $
-              últimosU ("entrada1") (2::Int) `shouldBe` ("a1"::String)
-            it "últimosU" $
-              últimosU ("entrada1"::String) (100::Int) `shouldBe` ("entrada1"::String)
-            it "subStringDeAteAppend" $
-              subStringDeAteAppend ("entrada1"::String) ("entrada2"::String) 2 2 `shouldBe` ("trtr"::String)
-            it "subStringDeAteAppend" $
-              subStringDeAteAppend ("entr"::String) ("ada2"::String) 0 4 `shouldBe` ("entrada2"::String)
-
-          describe "Teste de tuplas" $ do
-            it "naipe" $
-              naipe (3, "ouro") `shouldBe` "ouro"
-            it "naipe" $
-              naipe (3, "copas") `shouldBe` "copas"
-            it "valor" $
-              valor (3, "ouro") `shouldBe` 3
-            it "valor" $
-              valor (4, "ouro") `shouldBe` 4
-
-            it "menor" $
-              cartaÉMenor (1, "ouro") (1, "ouro") `shouldBe` False 
-            it "menor" $
-              cartaÉMenor (1, "copas") (1, "ouro") `shouldBe` True 
-            it "menor" $
-              cartaÉMenor (1, "ouro") (2, "ouro") `shouldBe` True
-
-            it "igual" $
-              cartaÉIgual (1, "ouro") (1, "ouro") `shouldBe` True 
-            it "igual" $
-              cartaÉIgual (1, "copas") (1, "ouro") `shouldBe` False 
-            it "igual" $
-              cartaÉIgual (1, "ouro") (2, "ouro") `shouldBe` False
-
-            it "maior" $
-              cartaÉMaior (1, "ouro") (1, "ouro") `shouldBe` False 
-            it "maior" $
-              cartaÉMaior (1, "copas") (1, "ouro") `shouldBe` False 
-            it "maior" $
-              cartaÉMaior (1, "ouro") (2, "ouro") `shouldBe` False
-            it "maior" $
-              cartaÉMaior (3, "ouro") (2, "ouro") `shouldBe` True
-
-            it "jogo" $
-              éJogo (1,"ouro") (2,"ouro") (3,"ouro") `shouldBe` True 
-            it "jogo" $
-              éJogo (1,"ouro") (1,"ouro") (1,"copas") `shouldBe` False 
-            it "jogo" $
-              éJogo (1,"ouro") (1,"paus") (1,"copas") `shouldBe` True
-            it "jogo" $
-              éJogo (6,"paus") (4,"ouro") (3,"copas") `shouldBe` False
-              
-            it "maior mao empate" $
-              maiorMão ((5,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(3,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(3,"copas"))
-            it "maior mao primeira" $
-              maiorMão ((5,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(2,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(3,"copas"))
-            it "maior mao primeira" $
-              maiorMão ((5,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"copas"),(3,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(3,"copas")) 
-            it "maior mao segunda" $
-              maiorMão ((4,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(2,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(2,"copas")) 
-
-            it "mãoGanhadora empate" $
-             mãoGanhadora ((5,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(3,"copas")) `shouldBe` ((0,""),(0,""),(0,""))
-            it "maior mao primeira" $
-              mãoGanhadora ((5,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(2,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(3,"copas"))
-            it "maior mao primeira ganhadora segunda" $
-              mãoGanhadora ((6,"paus"),(4,"ouro"),(3,"copas")) ((3,"paus"),(3,"ouro"),(3,"copas")) `shouldBe` ((3,"paus"),(3,"ouro"),(3,"copas")) 
-            it "maior mao segunda" $
-              mãoGanhadora ((4,"paus"),(4,"ouro"),(3,"copas")) ((5,"paus"),(4,"ouro"),(2,"copas")) `shouldBe` ((5,"paus"),(4,"ouro"),(2,"copas")) 
+          describe "Com casamento de padrões" $ do
+            it "Naipe 1 " $
+              naipe (1,Ouro) `shouldBe` Ouro
+            it "Naipe 2 " $
+              naipe (1,Copas) `shouldBe` Copas
+            it "Valor 1 " $
+              valor (1,Ouro) `shouldBe` 1
+            it "Valor 2 " $
+              valor (2,Copas) `shouldBe` 2
+            it "Naipe igual 1" $
+              naipeIgual (1,Ouro) (2,Ouro) `shouldBe` True
+            it "Naipe igual 2" $
+              naipeIgual (1,Ouro) (2,Paus) `shouldBe` False
+            it "Valor por extenso 1" $
+              valorPorExtenso (1,Ouro) `shouldBe` "Um"
+            it "Valor por extenso 2" $
+              valorPorExtenso (2,Ouro) `shouldBe` "Dois"
+            it "Valor por extenso 3" $
+              valorPorExtenso (12,Ouro) `shouldBe` "Dama"
+            it "Naipe por extenso 1" $
+              naipePorExtenso (1,Ouro) `shouldBe` "Ouro"
+            it "Naipe por extenso 2" $
+              naipePorExtenso (2,Paus) `shouldBe` "Paus"
+            it "Naipe por extenso 3" $
+              naipePorExtenso (12,Copas) `shouldBe` "Copas"
+            it "Sequencia de naipes 1" $
+              sequênciaDeNaipes (1,Paus) (2,Ouro) (7,Copas) `shouldBe` False
+            it "Sequencia de naipes 2" $
+              sequênciaDeNaipes (1,Paus) (7,Copas) (2,Ouro) `shouldBe` False
+            it "Sequencia de naipes 3" $
+              sequênciaDeNaipes (1,Paus) (2,Espada) (7,Copas) `shouldBe` True
+            it "Sequencia de naipes 4" $
+              sequênciaDeNaipes (1,Espada) (2,Copas) (7,Ouro) `shouldBe` True
+            it "Data por extenso 1" $
+              dataPorExtenso 1 1 2001 `shouldBe` "Primeiro de Janeiro de 2001"
+            it "Data por extenso 2" $
+              dataPorExtenso 10 3 2010 `shouldBe` "Dez de Março de 2010"
