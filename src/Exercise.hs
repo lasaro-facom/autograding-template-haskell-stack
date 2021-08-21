ec7ad17cc57ac109140a4c9c14acce8c59cc6630
lasaro <lasaro@users.noreply.github.com>
2021-08-21 12:37:15 -0300

    exercícios

--- src/Exercise.hs
+++ src/Exercise.hs
@@ -1,179 +1,248 @@
 module Exercise where
 
 {- 
-Nos seguintes exercícios, implemente suas soluções de forma recursiva
-Defina os tipos das funções.
+Os seguintes exercícios, use listas para resolver os problemas propostos.
 -}
 
 
 {-
-Uma função que calcule x * y
+Um palíndromo é uma palavra que pode ser lida da esquerda para a direita ou da direita para a esquerda com o mesmo resultado, como por exemplo, ovo e Natan.
+O conceito pode ser estendido para frases se ignorarmos espaços, acentos e sinais de pontuação, por exemplo, "Olé! Maracujá, caju, caramelo." mas, por enquanto, estamos interessados apenas palavras palíndromas.
+
+Escreva uma função que teste se a entrada é uma palavra (não contém espaços).
+Assuma que a entrada não tem acentos ou pontuações e que ou todas as letras são maiúsculas ou todas são minúsculas.
 
 Entrada:
-    - x
-    - y
+    - s - String
 
 Resultado: 
-    - x * y
+    - True se s é uma palavra palíndroma; False caso contrário.
 
 Exemplos:
->>>multiplique 2 3
-6
->>>multiplique 3 3
-9
+>>>palíndromo "ana"
+True
+>>>palíndromo "jose"
+False
 
->>>multiplique 3 (-3)
--9
+>>>palíndromo "natan foi viajar"
+False
 
->>>multiplique (-3) 3
--9
-
->>>multiplique (-3) (-3)
-9
-
+>>>palíndromo "natan"
+True
 -}
 
-multiplique :: Int -> Int -> Int
-multiplique x n
-    | n == 0 = 0
-    | n == 1 = x
-    | n > 1 = x + multiplique x (n-1)
-    | otherwise = negate $ multiplique x (negate n)
+palíndromo :: String -> Bool
+palíndromo s = s == reverse s
 
 {-
-Uma função que calcule a n-ésima potência de um número x.
+Na próxima semana, escreva uma função que teste se uma frase é palíndroma.
+-}
+
+
+{-
+O conceito de palíndromo pode ser aplicado a qualquer lista, por exemplo de inteiros. Neste caso, [1,2,3,4,5,4,3,2,1] seria um palíndromo.
+Escreva uma função que teste se uma lista qualquer é um palíndromo.
+O tipo da função está definido para você e diz que a lista pode ser de qualquer tipo que se possa comparar com um ==
 
 Entrada:
-    - x: base
-    - n: expoente
+    - s - lista de inteiros
 
 Resultado: 
-    - x elevado a n
+    - True se s é uma lista palíndroma; False caso contrário.
 
 Exemplos:
->>>potência 2 3
-8
->>>potência (-3) 2
-9
--}
+>>>palindrome "ana"
+True
+>>>palindrome "jose"
+False
 
-potência :: Int -> Int -> Int
-potência _ 0 = 1
-potência x n = x * potência x (n-1)
+>>>palindrome [1,2,3]
+False
 
-{-
-Uma função que calcule log base 2 de n
-
-Entrada:
-    - n
-
-Resultado: 
-    - log_2 (n)
-
-Exemplos:
->>>logBase2 100
-6
-
->>>logBase2 16
-4
+>>>palindrome [True,False,False,True]
+True
 
 -}
-logBase2 1 = 0
-logBase2 n = 1 + logBase2 (n `div` 2)
+
+palindrome :: Eq a => [a] -> Bool
+palindrome s = s == reverse s
 
 
 {-
-Uma função que rotacione os elementos de uma tupla n vezes.
+Frequentemente precisamos limpar dados entrados por usuários em fomulários.
+Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em " José de Abreu  "
+Algumas linguagens tem uma função trim que remove tais espaços em branco.
+
+Implemente uma função que elimine todos os espaços em branco no início e fim de uma string.
+
+>>>trim "Implemente uma"
+"Implemente uma"
+
+>>>trim " Implemente uma "
+"Implemente uma"
+
+>>>trim "  Implemente uma  "
+"Implemente uma"
+
+>>>trim "   Implemente uma"
+"Implemente uma"
+
+>>>trim "Implemente uma   "
+"Implemente uma"
+-}
+trim :: String -> String
+trim s = reverse (dropWhile (== ' ') (reverse (dropWhile (== ' ') s)))
+
+
+{-
+Eu não tenho uma historia bonitinha para esta função, então vamos direto ao ponto.
+Escreva uma função que quebre uma String em todo lugar em que aparecer um certo caractere.
 
 Entrada:
-    - t: tupla de 5 inteiros.
-    - n: número de rotações a ser feito. Rotacionar à direita se n é positivo e a esquerda se n é negativo.
+    - l - String a ser dividida
+    - d - delimitador
 
-Resultado: 
-    - t rotacionado n vezes.
+Saída:
+    - Lista de strings resultante da divisão.
 
-Exemplos:
->>>rotacionar (1,2,3,4,5) 2
-(4,5,1,2,3)
->>>rotacionar (1,2,3,4,5) (-2)
-(3,4,5,1,2)
+>>>splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " ' '
+["Por","exemplo,","precisamos","tirar","os","espa\231os","no","in\237cio","e","fim","dos","dados","digitados,","como","em","Jos\233","de","Abreu",""]
+
+>>>splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " 'i'
+["Por exemplo, prec","samos t","rar os espa\231os no ","n\237c","o e f","m dos dados d","g","tados, como em Jos\233 de Abreu "]
+
+>>>splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " 'a'
+["Por exemplo, precis","mos tir","r os esp","\231os no in\237cio e fim dos d","dos digit","dos, como em Jos\233 de Abreu "]
 -}
+splitTodos :: [Char] -> Char -> [[Char]]
+splitTodos l d = if d `elem` l then primeiro : splitTodos segundo d else [l]
+    where primeiro = takeWhile (/= d) l
+          segundo = dropWhile (== d) (dropWhile (/= d) l)
 
-rotacionar :: (b, b, b, b, b) -> Int -> (b, b, b, b, b)
-rotacionar t 0 = t
-rotacionar (u,d,t,q,c) n
-    | n > 0 = rotacionar (c,u,d,t,q) (n-1)
-    | otherwise = rotacionar (d,t,q,c,u) (n+1)
 
 
 
 {-
-Uma função que jogue fora os caracteres inicias de uma string s até que o restante da string se inicie com um caractere c ou que a string fique fazia.
+Escreva uma função que retorne duplas formadas pelos por elementos das duas metades da lista, sendo o primeiro elemento do resultado formado pelo 
+primeiro elemento da primeira metade da lista mais o primeiro da segunda metade da lista, o segundo elemento formado pelo segundo elemento da primeira
+metade mais o segundo elemento da segunda metade e assim por diante.
 
-Entrada:
-    - s: string
-    - c: caractere.
+>>>combinaMetades [1,2,3,4,5,6]
+[(1,4),(2,5),(3,6)]
 
-Resultado: 
-    - a string resultante.
-
-Exemplos:
->>>jogarForaAté "Eu quis dizer, você não quis escutar." ','
-", voc\234 n\227o quis escutar."
-
->>>jogarForaAté "Eu quis dizer, você não quis escutar." 'z'
-"zer, voc\234 n\227o quis escutar."
-
->>>jogarForaAté "Eu quis dizer, você não quis escutar." 'v'
-"voc\234 n\227o quis escutar."
+>>>combinaMetades [1,2,3,4,5,6,7]
+[(1,4),(2,5),(3,6)]
 
 -}
 
-jogarForaAté :: [Char] -> Char -> [Char]
-jogarForaAté s c
-  | 0 == length s = s
-  | s !! 0 == c = s
-  | otherwise = jogarForaAté (drop 1 s) c
+combinaMetades :: [a] -> [(a,a)]
+combinaMetades l = zip primeiraMetade segundaMetade
+    where (primeiraMetade, segundaMetade) = splitAt (length l `div` 2) l
 
 
 {-
-A fórmula de Leibniz para pi (http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80)
-estabelece que a constante pode ser calculada como a série
+Escreva uma função que reverta combinaMetades. Ou seja
+>>>descombinaMetades [(1,4),(2,5),(3,6)] 
+[1,2,3,4,5,6,7]
+-}
+descombinaMetades :: [(a,a)] -> [a]
+descombinaMetades l = descombinaMetadesInterno l [] []
+    where descombinaMetadesInterno [] p1 p2 = reverse p1 ++ reverse p2
+          descombinaMetadesInterno ((e1,e2):es) p1 p2 = descombinaMetadesInterno es (e1:p1) (e2:p2)
 
-pi = (4/1) - (4/3) + (4/5) - (4/7)...
 
-Implemente uma função recursiva que calcule a constante até uma quantidade n de termos.
+{-
+Escreva uma função separe repetições consecutivas dentro de uma lista.
 
-Entrada:
-    - n: quantidade de termos
+>>>empacote "aaaabccaadeeee"
+["aaaa","b","cc","aa","d","eeee"]
 
-Resultado:
-    - pi, calculado com n passos da série
+>>>empacote ""
+[]
 
-Exemplos:
 
->>>piDeLeibniz 1
-4.0
-
->>>piDeLeibniz 2
-2.666666666666667
-
->>>piDeLeibniz 3
-3.466666666666667
-
->>>piDeLeibniz 2000
-3.1410926536210413
-
->>>piDeLeibniz 3000
-3.1412593202657186
+>>>empacote [1,1,12,2,2,3,3,3,4,4,4,3,3,3,2,2,2,1,1,1]
+[[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]]
 
 -}
 
-piDeLeibniz :: Int -> Double
-piDeLeibniz = piDeLeibniz' (4::Double) 1
+empacote :: (Eq a) => [a] -> [[a]]
+empacote [] = []
+empacote (x:xs) = pacote:resto
+    where pacote = x:takeWhile (==x) xs
+          resto = empacote (dropWhile (==x) xs)
 
-piDeLeibniz' :: Double -> Int -> Int -> Double
-piDeLeibniz' piAteAgora termo contador
-    | termo == contador = piAteAgora
-    | otherwise  = piDeLeibniz' (piAteAgora + if even termo  then 4.0 / (2.0 * termoAsDouble + 1.0) else negate 4.0/(2.0* termoAsDouble + 1.0)) (termo +1) contador
-    where termoAsDouble = fromIntegral termo :: Double
+empacote' :: (Eq a) => [a] -> [[a]]
+empacote' l
+    | null l = []
+    | otherwise  = pacote:resto
+    where
+          cabeça = head l
+          cauda = tail l
+          pacote = cabeça:takeWhile (==cabeça) cauda
+          resto = empacote (dropWhile (==cabeça) cauda)
+
+{-
+Dado uma lista empacotada, como a gerada pela função anterior, gere uma lista de duplas tal que:
+- para cada pacote haja uma dupla no resultado.
+- a dupla tem como primeiro elemento o dado repetido na lista correspondente e como segundo elemento o comprimento de tal lista.
+
+>>>compacte [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]]
+[(1,2),(12,1),(2,2),(3,3),(4,3),(3,3),(2,3),(1,3)]
+
+>>>compacte ["aaaa","b","cc","aa","d","eeee"]
+[('a',4),('b',1),('c',2),('a',2),('d',1),('e',4)]
+
+>>>compacte []
+Ambiguous type variable ‘a0’ arising from a use of ‘compacte’
+prevents the constraint ‘(Show a0)’ from being solved.
+Relevant bindings include
+  it :: [(a0, Int)]
+    (bound at /Users/lasarocamargos/ufu/github-classroom/autograding-template-haskell-stack/src/Exercise.hs:189:1)
+Probable fix: use a type annotation to specify what ‘a0’ should be.
+These potential instances exist:
+  instance Show a => Show (ZipList a)
+    -- Defined in ‘Control.Applicative’
+  instance Show NestedAtomically
+    -- Defined in ‘Control.Exception.Base’
+  instance Show NoMethodError -- Defined in ‘Control.Exception.Base’
+  ...plus 221 others
+  (use -fprint-potential-instances to see them all)
+
+>>>compacte [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3]]
+[(1,2),(12,1),(2,2),(3,3),(4,3),(3,3)]
+
+-}
+compacte :: [[a]] -> [(a, Int)]
+compacte l
+    | null l = []
+    | otherwise = (head cabeca, length cabeca) : compacte cauda
+        where cabeca = head l
+              cauda = tail l
+
+
+{-
+Escreva uma função que reverta a função compacte, definida acima, ou seja, tal que
+>>>descompacte (compacte [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3]]) == [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3]]
+True
+
+-}
+
+descompacte :: [(a, Int)] -> [[a]]
+descompacte l
+    | null l = []
+    | otherwise = pacote cabeca : descompacte cauda
+        where cabeca = head l
+              cauda = tail l
+              pacote (c,cont) = replicate cont c
+
+
+{-
+Escreva uma função que reverta a função empacote, acima, definida acima, ou seja, tal que
+>>>desempacote (empacote "aaaabccaadeeee") == "aaaabccaadeeee"
+True
+
+-}
+
+desempacote :: [[a]] -> [a]
+desempacote = concat
--- test/Tests.hs
+++ test/Tests.hs
@@ -8,44 +8,81 @@ main = hspecWith defaultConfig specs
 
 specs :: Spec
 specs = do
-          describe "Com casamento de padrões" $ do
-            it "2 x 3" $
-              multiplique 2 3 `shouldBe` 6
-            it "3 x 3" $
-              multiplique 3 3 `shouldBe` 9
-            it "-3 x 3" $
-              multiplique (-3) 3 `shouldBe` -9
-            it "3 x -3" $
-              multiplique 3 (-3) `shouldBe` -9
-            it "-3 x -3" $
-              multiplique (-3) (-3) `shouldBe` 9
-            it "potência 2 3" $
-              potência 2 3 `shouldBe` 8
-            it "potência -3 2" $
-              potência (-3) 2 `shouldBe` 9
-            it "potência -3 4" $
-              potência (-3) 3 `shouldBe` (-27)
-            it "log2 100" $
-              logBase2 100 `shouldBe` 6
-            it "log2 16" $
-              logBase2 16 `shouldBe` 4
-            it "rotacionar 1 a 5, 2" $
-              shouldBe (rotacionar (1,2,3,4,5) 2) (4,5,1,2,3)
-            it "rotacionar 1 a 5, -1" $
-              shouldBe (rotacionar (1,2,3,4,5) (-2)) (3,4,5,1,2)
-            it "drop ," $
-              shouldBe (jogarForaAté "Eu quis dizer, você não quis escutar." ',') ", voc\234 n\227o quis escutar."
-            it "drop z" $
-              shouldBe (jogarForaAté "Eu quis dizer, você não quis escutar." 'z') "zer, voc\234 n\227o quis escutar."
-            it "drop v" $
-              shouldBe (jogarForaAté "Eu quis dizer, você não quis escutar." 'v') "voc\234 n\227o quis escutar."
-            it "pi 1" $
-              shouldBe (piDeLeibniz 1) 4.0
-            it "pi 2" $
-              shouldBe (piDeLeibniz 2) 2.666666666666667
-            it "pi 3" $
-              shouldBe (piDeLeibniz 3) 3.466666666666667
-            it "pi 2000" $
-              shouldBe (piDeLeibniz 2000) 3.1410926536210413
-            it "pi 3000" $
-              shouldBe (piDeLeibniz 3000) 3.1412593202657186
+          describe "Funções recursivas, com listas e where ou let-in" $ do
+            it "palíndromo ana" $
+              palíndromo "ana" `shouldBe` True
+            it "palíndromo jose" $
+              palíndromo "jose" `shouldBe` False
+            it "palíndromo natan" $
+              palíndromo "natan" `shouldBe` True
+            it "palíndromo natan 2" $
+              palíndromo "natan foi viajar" `shouldBe` False
+            it "palindrome 123" $
+              palindrome [1,2,3] `shouldBe` False
+            it "palindrome 12321" $
+              palindrome [1,2,3,2,1] `shouldBe` True
+            it "palindrome 123321" $
+              palindrome [1,2,3,3,2,1] `shouldBe` True
+            it "palindrome boolean" $
+              palindrome [True,False,False,True] `shouldBe` True
+
+            it "trim 1" $
+              trim "Implemente uma" `shouldBe` "Implemente uma"
+            it "trim 2" $
+              trim " Implemente uma " `shouldBe` "Implemente uma"
+            it "trim 3" $
+              trim "  Implemente uma  " `shouldBe` "Implemente uma"
+            it "trim 4" $
+              trim "   Implemente uma" `shouldBe` "Implemente uma"
+            it "trim 5" $
+              trim "Implemente uma   " `shouldBe` "Implemente uma"
+
+            it "split 1" $
+              splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " ' ' `shouldBe` ["Por","exemplo,","precisamos","tirar","os","espa\231os","no","in\237cio","e","fim","dos","dados","digitados,","como","em","Jos\233","de","Abreu",""]
+            it "split 2" $
+              splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " 'i' `shouldBe` ["Por exemplo, prec","samos t","rar os espa\231os no ","n\237c","o e f","m dos dados d","g","tados, como em Jos\233 de Abreu "]
+            it "split 3" $
+              splitTodos "Por exemplo, precisamos tirar os espaços no início e fim dos dados digitados, como em José de Abreu " 'a' `shouldBe` ["Por exemplo, precis","mos tir","r os esp","\231os no in\237cio e fim dos d","dos digit","dos, como em Jos\233 de Abreu "]
+
+            it "combina 1" $
+              combinaMetades [1,2,3,4,5,6] `shouldBe` [(1,4),(2,5),(3,6)]
+            it "combina 2" $
+              combinaMetades [1,2,3,4,5,6,7] `shouldBe` [(1,4),(2,5),(3,6)]
+
+            it "descombina 1" $
+              descombinaMetades [(1,4),(2,5),(3,6)] `shouldBe`  [1,2,3,4,5,6]
+            it "descombina 2" $
+              descombinaMetades [(1,4),(2,5),(3,6),(10,11)] `shouldBe` [1,2,3,10,4,5,6,11] 
+
+            it "empacote 1" $
+              empacote "aaaabccaadeeee" `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
+            it "empacote 2" $              
+              empacote "" `shouldBe` []
+            it "empacote 3" $
+              empacote [1,1,12,2,2,3,3,3,4,4,4,3,3,3,2,2,2,1,1,1] `shouldBe` [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]]
+
+            it "compacte 1" $
+              compacte [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]] `shouldBe` [(1,2),(12,1),(2,2),(3,3),(4,3),(3,3),(2,3),(1,3)]
+            it "compacte 2" $
+              compacte ["aaaa","b","cc","aa","d","eeee"] `shouldBe` [('a',4),('b',1),('c',2),('a',2),('d',1),('e',4)]
+            it "compacte 3" $
+              compacte ([]::[String]) `shouldBe` ([]::[(Char,Int)])
+            it "compacte 4" $
+              compacte [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3]] `shouldBe` [(1,2),(12,1),(2,2),(3,3),(4,3),(3,3)]
+
+            it "descompacte 1" $
+              descompacte  [(1,2),(12,1),(2,2),(3,3),(4,3),(3,3),(2,3),(1,3)] `shouldBe` [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]]
+            it "descompacte 2" $
+              descompacte [('a',4),('b',1),('c',2),('a',2),('d',1),('e',4)] `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
+            it "descompacte 3" $
+              descompacte ([]::[(Char,Int)]) `shouldBe` ([]::[String])
+            it "descompacte 4" $
+              descompacte [(1,2),(12,1),(2,2),(3,3),(4,3),(3,3)] `shouldBe` [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3]] 
+
+
+            it "desempacote 1" $
+              desempacote ["aaaa","b","cc","aa","d","eeee"] `shouldBe` "aaaabccaadeeee" 
+            it "empacote 2" $              
+              desempacote ([]::[String]) `shouldBe` ([]::String)
+            it "desempacote 3" $
+              desempacote [[1,1],[12],[2,2],[3,3,3],[4,4,4],[3,3,3],[2,2,2],[1,1,1]] `shouldBe` [1,1,12,2,2,3,3,3,4,4,4,3,3,3,2,2,2,1,1,1] 
